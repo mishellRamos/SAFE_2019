b91a27c5ce66f46011e6d8647788e5fe3dbc75cb
Unai <unai.dominguez.ihs@gmail.com>
2019-02-26 16:09:40 +0100

    Proyecto SAFE creado

---
+++ SAFE/app/src/main/java/com/aro/jru/safe/UrgenciaActivity.java
@@ -0,0 +1,13 @@
+package com.aro.jru.safe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UrgenciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_urgencia);
+    }
+}
